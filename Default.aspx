d09988089300efbbcf6e0f2ff0ca4bfe0a1c13f5
kribula <kbl@fuzzyweb.dk>
2015-12-23 11:46:48 +0100

    Sample Code

---
+++ Default.aspx
@@ -0,0 +1,156 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Hubic.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h1>hubiC sample code</h1>
+    <p>hubiC is a cloud storage system and it has an API so you can make integration from your own programs</p>
+    <h3><a href="https://api.hubic.com/" target="_blank">API description</a></h3>
+        <hr />
+        <p>First thing to do is to login to your hubiC account (or create a new account - you get 25GB storage for free) and create an application.<br />
+           Store the Client ID, Secret Client and Redirection domain in the web.config. They should be displayed here:</p>
+        <p>
+            <asp:Label ID="Label13" runat="server" Text="Client ID:"></asp:Label>
+            <asp:TextBox ID="TextBoxClientID" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label14" runat="server" Text="Secret Client:"></asp:Label>
+            <asp:TextBox ID="TextBoxSecretClient" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label15" runat="server" Text="Redirection domain:"></asp:Label>
+            <asp:TextBox ID="TextBoxRedirectionDomain" runat="server" Width="400px"></asp:TextBox><br />
+            <small>Note: To test on your localhost you must map this domain temporarely to 127.0.0.1 in your hosts file (C:\Windows\System32\drivers\etc\hosts).</small>
+        </p>
+        <hr />
+        <p>With the Client ID and Redirection domain as parameters we call the hubiC login page.<br />
+           Here you must login with your credentials and then you are redirected back to this site with an request code</p>
+        <asp:Button ID="ButtonGetRequestCode" runat="server" OnClick="ButtonGetRequestCode_Click" Text="Get Request Code" />
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Code:"></asp:Label>
+            <asp:TextBox ID="TextBoxCode" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Scope:"></asp:Label>
+            <asp:TextBox ID="TextBoxScope" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <hr />
+        <p>From the request code you can get an access token which you must use when you call the methods in the API</p>
+        <asp:Button ID="ButtonGetAccessToken" runat="server" OnClick="ButtonGetAccessToken_Click" Text="Get Access Token" Width="155px" />
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Access Token:"></asp:Label>
+            <asp:TextBox ID="TextBoxAccessToken" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Expires in (milliseconds):"></asp:Label>
+            <asp:TextBox ID="TextBoxExpiresIn" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="Refresh Token:"></asp:Label>
+            <asp:TextBox ID="TextBoxRefreshToken" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label6" runat="server" Text="Token type:"></asp:Label>
+            <asp:TextBox ID="TextBoxTokenType" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <hr />
+        <p>The access token expires after a short amount of time but you can use the refresh token to get a new access token:</p>
+        <asp:Button ID="ButtonRefreshAccessToken" runat="server" OnClick="ButtonRefreshAccessToken_Click" Text="Refresh Access Token" Width="155px" />
+        <hr />
+        <p>With the access token you can get some information about the user account</p>
+        <p>
+            <asp:Button ID="ButtonGetAccount" runat="server" OnClick="ButtonGetAccount_Click" Text="Get Account" />
+        </p>
+        <p>
+            <asp:Label ID="Label7" runat="server" Text="Email:"></asp:Label>
+            <asp:TextBox ID="TextBoxEmail" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label8" runat="server" Text="Creation Date:"></asp:Label>
+            <asp:TextBox ID="TextBoxCreationDate" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label9" runat="server" Text="Status:"></asp:Label>
+            <asp:TextBox ID="TextBoxStatus" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <hr />
+        <p>And you can get some information about used space and quota:</p>
+        <p>
+            <asp:Button ID="ButtonGetUsage" runat="server" OnClick="ButtonGetUsage_Click" Text="Get Usage" />
+        </p>
+        <p>
+            <asp:Label ID="Label16" runat="server" Text="Quota:"></asp:Label>
+            <asp:TextBox ID="TextBoxQuota" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label17" runat="server" Text="Space used:"></asp:Label>
+            <asp:TextBox ID="TextBoxSpaceUsed" runat="server" Width="400px"></asp:TextBox>
+        </p>
+        <hr />
+        <p>To access the file storage you must first get an endpoint and a token:</p>
+        <asp:Button ID="ButtonGetCredentials" runat="server" OnClick="ButtonGetCredentials_Click" Text="Get Credentials" Width="113px" />
+        <p>
+            <asp:Label ID="Label10" runat="server" Text="Token:"></asp:Label>
+            <asp:TextBox ID="TextBoxToken" runat="server" Width="350px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label11" runat="server" Text="Endpoint:"></asp:Label>
+            <asp:TextBox ID="TextBoxEndpoint" runat="server" Width="350px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label12" runat="server" Text="Expires:"></asp:Label>
+            <asp:TextBox ID="TextBoxExpires" runat="server" Width="350px"></asp:TextBox>
+        </p>
+        <hr />
+        <p>Then you can use this endpoint and token to access containers and objects (files):</p>
+        <table>
+            <tr>
+                <td>
+                    <asp:Button ID="ButtonListContainers" runat="server" OnClick="ButtonListContainers_Click" Text="List containers" Width="200px" />
+                    <br />
+                    <asp:ListBox ID="ListBoxContainers" runat="server" Width="295px" Height="141px"></asp:ListBox>
+                </td>
+                <td>
+                    <asp:Label ID="Label18" runat="server" Text="New container name:"></asp:Label>
+                    <br />
+                    <asp:TextBox ID="TextBoxNewContainerName" runat="server">NewContainerName</asp:TextBox>
+                    <br />
+                    <asp:Button ID="ButtonCreateContainer" runat="server" OnClick="ButtonCreateContainer_Click" Text="Create container" Width="200px" />
+                    <br />
+                    <br />
+                    <asp:Button ID="ButtonDeleteSelectedContainer" runat="server" OnClick="ButtonDeleteSelectedContainer_Click" Text="Delete selected container" Width="200px" />
+                    <br />
+                    <small>Note: To delete a container it must be empty.</small>
+                </td>
+            </tr>   
+        </table>
+        <table>
+            <tr>
+                <td>
+                    <asp:Button ID="ButtonListFiles" runat="server" Text="List files in selected container" Width="200px" OnClick="ButtonListFiles_Click" />
+                    <br />
+                    <asp:ListBox ID="ListBoxObjects" runat="server" Width="295px" Height="141px"></asp:ListBox>
+                </td>
+                <td>
+                    <asp:Label ID="Label19" runat="server" Text="Upload new file to selected container:"></asp:Label>
+                    <br />
+                    <asp:FileUpload ID="FileUpload1" runat="server" Width="348px" />
+                    <br />
+                    <asp:Button ID="ButtonUploadFile" runat="server" Text="Upload file" Width="200px" OnClick="ButtonUploadFile_Click" />
+                    <br />
+                    <br />
+                    <asp:Button ID="ButtonDownload" runat="server" OnClick="ButtonDownload_Click" Text="Download selected file" Width="200px" />
+                    <br />
+                    <br />
+                    <asp:Button ID="ButtonDeleteSelectedFile" runat="server" OnClick="ButtonDeleteSelectedFile_Click" Text="Delete selected file" Width="200px" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hubic
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string hubic_auth_url = "https://api.hubic.com/oauth/auth/?client_id={0}&redirect_uri={1}&scope={2}&response_type={3}&state={4}";
+        private string hubic_auth_token_url = "https://api.hubic.com/oauth/token";
+        private string hubic_account_url = "https://api.hubic.com/1.0/account";
+        private string hubic_usage_url = "https://api.hubic.com/1.0/account/usage";
+        private string hubic_credentials_url = "https://api.hubic.com/1.0/account/credentials";
+
+        private string Client_ID 
+        {
+            get
+            {
+                return TextBoxClientID.Text;
+            }
+        }
+
+        private string SecretClient
+        {
+            get
+            {
+                return TextBoxSecretClient.Text;
+            }
+        }
+
+        private string RedirectionDomain
+        {
+            get
+            {
+                return TextBoxRedirectionDomain.Text;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // If parameter state=code in query string then this is the redirect from the 
+            // hubiC login page with the request code and scope
+            string state = Request.QueryString["state"];
+            switch (state)
+            {
+                case "code":
+                    TextBoxCode.Text = Request.QueryString["code"];
+                    TextBoxScope.Text = Request.QueryString["scope"];
+                    break;
+            }
+
+            // Try to load Client ID, Secret Client and Redirection Domain from web.config
+            if (string.IsNullOrEmpty(TextBoxClientID.Text))
+            {
+                TextBoxClientID.Text = System.Configuration.ConfigurationManager.AppSettings["ClientID"];
+            }
+            if (string.IsNullOrEmpty(TextBoxSecretClient.Text))
+            {
+                TextBoxSecretClient.Text = System.Configuration.ConfigurationManager.AppSettings["SecretClient"];
+            }
+            if (string.IsNullOrEmpty(TextBoxRedirectionDomain.Text))
+            {
+                TextBoxRedirectionDomain.Text = System.Configuration.ConfigurationManager.AppSettings["RedirectionDomain"];
+            }
+        }
+
+        protected void ButtonGetRequestCode_Click(object sender, EventArgs e)
+        {
+            // Redirect to the hubiC login page to get the request code
+            // When you have entered your credentials you are redirected back 
+            // to this site and you can get the request code from the query string
+            string response_type = "code";
+            string scope = "usage.r,account.r,getAllLinks.r,credentials.r,sponsorCode.r,activate.w,sponsored.r,links.drw";
+            string state = response_type;
+
+            string url = string.Format(hubic_auth_url,
+                Client_ID, HttpUtility.UrlEncode(RedirectionDomain), scope, response_type, state);
+
+            Response.Redirect(url);
+        }
+
+        protected void ButtonGetAccessToken_Click(object sender, EventArgs e)
+        {
+            // From the request code you get the access token
+            NameValueCollection parameters = new NameValueCollection()
+            {
+                {"code", TextBoxCode.Text},
+                {"redirect_uri", RedirectionDomain},
+                {"grant_type", "authorization_code"}
+            };
+
+            string response = DoHttpPost(parameters);
+
+            JavaScriptSerializer jsonSerialize = new JavaScriptSerializer();
+            AccessToken accessToken = jsonSerialize.Deserialize<AccessToken>(response);
+
+            TextBoxAccessToken.Text = accessToken.access_token;
+            TextBoxExpiresIn.Text = accessToken.expires_in;
+            TextBoxRefreshToken.Text = accessToken.refresh_token;
+            TextBoxTokenType.Text = accessToken.token_type;
+        }
+
+        protected void ButtonRefreshAccessToken_Click(object sender, EventArgs e)
+        {
+            // Refresh the access token
+            NameValueCollection parameters = new NameValueCollection()
+            {
+                {"refresh_token", TextBoxRefreshToken.Text},
+                {"grant_type", "refresh_token"}
+            };
+
+            string response = DoHttpPost(parameters);
+
+            JavaScriptSerializer jsonSerialize = new JavaScriptSerializer();
+            AccessToken accessToken = jsonSerialize.Deserialize<AccessToken>(response);
+
+            TextBoxAccessToken.Text = accessToken.access_token;
+            TextBoxExpiresIn.Text = accessToken.expires_in;
+            TextBoxTokenType.Text = accessToken.token_type;
+        }
+
+        protected void ButtonGetAccount_Click(object sender, EventArgs e)
+        {
+            // Get account information
+            string response = DoHttpGet(hubic_account_url);
+            JavaScriptSerializer jsonSerialize = new JavaScriptSerializer();
+
+            Account account = jsonSerialize.Deserialize<Account>(response);
+
+            TextBoxEmail.Text = account.email;
+            TextBoxCreationDate.Text = account.creationDate;
+            TextBoxStatus.Text = account.status;
+        }
+
+        protected void ButtonGetUsage_Click(object sender, EventArgs e)
+        {
+            // Get usage information
+            string response = DoHttpGet(hubic_usage_url);
+            JavaScriptSerializer jsonSerialize = new JavaScriptSerializer();
+
+            Usage usage = jsonSerialize.Deserialize<Usage>(response);
+
+            TextBoxQuota.Text = usage.quota;
+            TextBoxSpaceUsed.Text = usage.used;
+        }
+
+        protected void ButtonGetCredentials_Click(object sender, EventArgs e)
+        {
+            // Get endpoint and token to the file storage
+            string response = DoHttpGet(hubic_credentials_url);
+            JavaScriptSerializer jsonSerialize = new JavaScriptSerializer();
+
+            Credentials credentials = jsonSerialize.Deserialize<Credentials>(response);
+
+            TextBoxToken.Text = credentials.token;
+            TextBoxEndpoint.Text = credentials.endpoint;
+            TextBoxExpires.Text = credentials.expires;
+        }
+
+        protected void ButtonListContainers_Click(object sender, EventArgs e)
+        {
+            // List the containers
+            ListContrainers();
+        }
+
+        protected void ButtonCreateContainer_Click(object sender, EventArgs e)
+        {
+            // Create a new container
+            string containername = TextBoxNewContainerName.Text;
+
+            string endpoint = TextBoxEndpoint.Text + "/" + containername;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("X-Auth-Token", token);
+                byte[] data = new byte[0];
+                byte[] responseData = wc.UploadData(endpoint, "PUT", data);
+                string response = System.Text.Encoding.UTF8.GetString(responseData);
+
+                ListContrainers();
+            }
+        }
+
+        protected void ButtonDeleteSelectedContainer_Click(object sender, EventArgs e)
+        {
+            // Delete a container
+            string containername = ListBoxContainers.SelectedItem.Text;
+
+            string endpoint = TextBoxEndpoint.Text + "/" + containername;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("X-Auth-Token", token);
+                byte[] data = new byte[0];
+                byte[] responseData = wc.UploadData(endpoint, "DELETE", data);
+                string response = System.Text.Encoding.UTF8.GetString(responseData);
+
+                ListContrainers();
+            }
+        }
+
+        protected void ButtonListFiles_Click(object sender, EventArgs e)
+        {
+            ListObjects();
+        }
+
+        protected void ButtonUploadFile_Click(object sender, EventArgs e)
+        {
+            byte[] fileData = FileUpload1.FileBytes;
+
+            string containername = ListBoxContainers.SelectedItem.Text;
+            string filename = FileUpload1.FileName;
+
+            
+            string endpoint = TextBoxEndpoint.Text + "/" + containername + "/" + filename;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("X-Auth-Token", token);
+                byte[] responseData = wc.UploadData(endpoint, "PUT", fileData);
+                string response = System.Text.Encoding.UTF8.GetString(responseData);
+
+                ListObjects();
+            }
+        }
+
+        protected void ButtonDownload_Click(object sender, EventArgs e)
+        {
+            string containername = ListBoxContainers.SelectedItem.Text;
+            string filename = ListBoxObjects.SelectedItem.Text;
+
+            string endpoint = TextBoxEndpoint.Text + "/" + containername + "/" + filename;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("X-Auth-Token", token);
+                byte[] data = wc.DownloadData(endpoint);
+
+                Response.OutputStream.Write(data, 0, data.Length);
+                Response.ContentType = "application/octet-stream";
+                Response.AddHeader("Content-Disposition", "attachment;filename=" + filename);
+                Response.End();
+            }           
+        }
+
+        protected void ButtonDeleteSelectedFile_Click(object sender, EventArgs e)
+        {
+            // Delete a file
+            string containername = ListBoxContainers.SelectedItem.Text;
+            string filename = ListBoxObjects.SelectedItem.Text;
+
+            string endpoint = TextBoxEndpoint.Text + "/" + containername + "/" + filename;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("X-Auth-Token", token);
+                byte[] data = new byte[0];
+                byte[] responseData = wc.UploadData(endpoint, "DELETE", data);
+                string response = System.Text.Encoding.UTF8.GetString(responseData);
+
+                ListObjects();
+            }
+        }
+
+        private void ListContrainers()
+        {
+            string endpoint = TextBoxEndpoint.Text;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+
+                wc.Headers.Add("X-Auth-Token", token);
+                using (StreamReader sr = new StreamReader(wc.OpenRead(endpoint)))
+                {
+                    string response = sr.ReadToEnd();
+                    string[] containers = response.Split('\n');
+                    ListBoxContainers.Items.Clear();
+                    foreach (string s in containers)
+                    {
+                        ListBoxContainers.Items.Add(s);
+                    }
+                    ListBoxContainers.Items[0].Selected = true;
+                }
+            }
+        }
+
+        private void ListObjects()
+        {
+            string container = ListBoxContainers.SelectedItem.Text;
+
+            string endpoint = TextBoxEndpoint.Text + "/" + container;
+            string token = TextBoxToken.Text;
+
+            using (WebClient wc = new WebClient())
+            {
+
+                wc.Headers.Add("X-Auth-Token", token);
+                using (StreamReader sr = new StreamReader(wc.OpenRead(endpoint)))
+                {
+                    string response = sr.ReadToEnd();
+                    string[] objects = response.Split('\n');
+                    ListBoxObjects.Items.Clear();
+                    if (!string.IsNullOrEmpty(response))
+                    {
+                        foreach (string s in objects)
+                        {
+                            ListBoxObjects.Items.Add(s);
+                        }
+                        ListBoxObjects.Items[0].Selected = true;
+                    }
+                }
+            }
+        }
+
+        // Helper method for doing a HTTP POST
+        private string DoHttpPost(NameValueCollection parameters)
+        {
+            string auth = "Basic " + Convert.ToBase64String(
+                System.Text.Encoding.UTF8.GetBytes(Client_ID + ":" + SecretClient));
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add(HttpRequestHeader.Authorization, auth);
+                byte[] response = wc.UploadValues(hubic_auth_token_url, parameters);
+                return System.Text.Encoding.UTF8.GetString(response);
+            }
+        }
+
+        // Helper method for doing a HTTP GET
+        private string DoHttpGet(string url)
+        {
+            string accessToken = TextBoxAccessToken.Text;
+            string accessTokenType = TextBoxTokenType.Text;
+            string auth = accessTokenType + " " + accessToken;
+
+            using (WebClient wc = new WebClient())
+            {
+
+                wc.Headers.Add(HttpRequestHeader.Authorization, auth);
+                using (StreamReader sr = new StreamReader(wc.OpenRead(url)))
+                {
+                    string response = sr.ReadToEnd();
+                    return response;
+                }
+            }
+        }
+
+        // Helper classes for deserializing json
+        private class AccessToken
+        {
+            public string access_token { get; set; }
+            public string expires_in { get; set; }
+            public string refresh_token { get; set; }
+            public string token_type { get; set; }
+        }
+
+        private class Account
+        {
+            public string email { get; set; }
+            public string creationDate { get; set; }
+            public string status { get; set; }
+        }
+
+        private class Usage
+        {
+            public string quota { get; set; }
+            public string used { get; set; }
+        }
+
+        private class Credentials
+        {
+            public string token { get; set; }
+            public string endpoint { get; set; }
+            public string expires { get; set; }
+        }
+    }
+}
